c48a5f0df49cdc0111832aa7929b7590381d4f6c
chen peiyu <chenpeiyu2001@outlook.com>
2023-01-23 22:23:31 +0800

    day04 实现了用户动态发布和订阅用户动态发布

---
+++ Imoc-bilibili-dao/src/main/java/com/bilibili/domain/Constant/AuthRoleConstant.java
@@ -0,0 +1,8 @@
+package com.bilibili.domain.Constant;
+
+public interface AuthRoleConstant {
+
+    public static final String ROLE_LV0 = "Lv0";
+
+    public static final String ROLE_LV1 = "Lv1";
+}
---
+++ Imoc-bilibili-dao/src/main/java/com/bilibili/domain/auth/AuthMenu.java
@@ -0,0 +1,23 @@
+package com.bilibili.domain.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthMenu {
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private Date createTime;
+
+    private Date updateTime;
+}
