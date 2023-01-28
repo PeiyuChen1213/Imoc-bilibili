27bba6fa2cedde908a7a5b43b1fc89729e081e90
chen peiyu <chenpeiyu2001@outlook.com>
2023-01-28 23:27:16 +0800

    day06 文件上传相关已经实现

--- Imoc-bilibili-dao/src/main/java/com/bilibili/domain/File.java
+++ Imoc-bilibili-dao/src/main/java/com/bilibili/domain/File.java
@@ -1,7 +1,15 @@
 package com.bilibili.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class File {
 
     private Long id;
